2910c198e071b32d3ecd382c98a032b7409a324c
mayrant <mayrant2004@yahoo.com>
2021-12-10 16:25:26 -0600

    new create bill controller

--- controllers/bills.js
+++ controllers/bills.js
@@ -33,19 +33,41 @@ const show = (req, res) => {
 };
 
 // create new bill
-const create = (req, res) => {
-  const { name, due_day, amount, fixed_amount } = req.body;
+// const create = (req, res) => {
+//   const { name, due_day, amount, fixed_amount } = req.body;
 
-  let sql = `INSERT INTO ?? VALUES (?, ?, ?, ?, ?, ?)`;
-  let replacements = ["bills", null, name, due_day, amount, fixed_amount, null];
+//   let sql = `INSERT INTO ?? VALUES (?, ?, ?, ?, ?, ?)`;
+//   let replacements = ["bills", null, name, due_day, amount, fixed_amount, null];
+//   sql = mysql.format(sql, replacements);
+
+//   pool.query(sql, (err, row) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).send("Oh no! Something went wrong.");
+//     }
+//     res.json(row);
+//   });
+// };
+
+const create = (req, res) => {
+  let newBill = {
+    name: req.body.name,
+    due_day: req.body.due_day,
+    amount: req.body.amount,
+    fixed_amount: req.body.fixed_amount,
+    user_id: req.body.user_id
+  }
+
+  let sql = `INSERT INTO bills (name, due_day, amount, fixed_amount, user_id) VALUES (?, ?, ?, ?, ?)`;
+  let replacements = [newBill.name, newBill.due_day, newBill.amount, newBill.fixed_amount, newBill.user_id];
   sql = mysql.format(sql, replacements);
 
-  pool.query(sql, (err, row) => {
+  pool.query(sql, function(err, row){
     if (err) {
       console.error(err);
       return res.status(500).send("Oh no! Something went wrong.");
     }
-    res.json(row);
+    res.json(newBill);
   });
 };
 
